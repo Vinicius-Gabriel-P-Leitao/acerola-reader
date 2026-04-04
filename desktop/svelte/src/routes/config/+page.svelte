73da1f2ba4c743cf815a0be65a465046ae59f4e9
Vinicius-Gabriel-P-Leitao <vinicius.gabriel.p.leitao@gmail.com>
2026-04-04 19:10:36 -0300

    [feat](desktop): adiciona estilo ao layout da tela de config e traduz.

--- desktop/svelte/src/routes/config/+page.svelte
+++ desktop/svelte/src/routes/config/+page.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
   import { useTheme } from "$lib/hooks/use-theme.svelte";
   import ThemePicker from "./components/theme-picker.svelte";
+  import { m } from "$lib/paraglide/messages";
 
   const ctx = useTheme();
 </script>
 
-<section class="p-8">
+<div class="max-w-5xl p-8 space-y-12">
+  <!-- Header -->
+  <div>
+    <h1 class="text-3xl font-bold tracking-tight text-foreground">
+      {m["pages.config.title"]()}
+    </h1>
+
+    <p class="text-muted-foreground mt-2">
+      {m["pages.config.desc"]()}
+    </p>
+  </div>
+
+  <!-- Aparência (Componente Existente) -->
   <ThemePicker theme={ctx.theme} mode={ctx.mode} onselect={ctx.setTheme} />
-</section>
+</div>
