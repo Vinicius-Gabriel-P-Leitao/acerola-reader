6b929fd7b0a4eff185359301394a700591947fac
Vinicius-Gabriel-P-Leitao <vinicius.gabriel.p.leitao@gmail.com>
2026-04-10 00:33:03 -0300

    [refactor](desktop): organiza o lib.rs para ter um formato mais organizado.

--- desktop/src-tauri/src/lib.rs
+++ desktop/src-tauri/src/lib.rs
@@ -3,83 +3,92 @@ mod core;
 mod data;
 mod infra;
 
-use commands::features::library::{comic_scanner_cmd, select_folder_cmd};
+use commands::features::library::{ comic_scanner_cmd, select_folder_cmd };
 use tauri::Manager;
 
 #[cfg(test)]
 pub mod tests;
 
-fn setup_opener(builder: tauri::Builder<tauri::Wry>) -> tauri::Builder<tauri::Wry> {
-    builder.plugin(tauri_plugin_opener::init())
-}
+mod app_bootstrap {
+    use super::*;
+    use std::path::PathBuf;
 
-fn setup_dialog(builder: tauri::Builder<tauri::Wry>) -> tauri::Builder<tauri::Wry> {
-    builder.plugin(tauri_plugin_dialog::init())
-}
+    pub fn build() -> tauri::Builder<tauri::Wry> {
+        let builder = tauri::Builder::default();
+        let builder = setup_opener(builder);
+        let builder = setup_dialog(builder);
+        let builder = setup_store(builder);
+        let builder = setup_sql(builder);
+        let builder = setup_fs(builder);
 
-fn setup_store(builder: tauri::Builder<tauri::Wry>) -> tauri::Builder<tauri::Wry> {
-    builder.plugin(tauri_plugin_store::Builder::new().build())
-}
-
-fn setup_fs(builder: tauri::Builder<tauri::Wry>) -> tauri::Builder<tauri::Wry> {
-    builder.plugin(tauri_plugin_fs::init())
-}
-
-fn setup_sql(builder: tauri::Builder<tauri::Wry>) -> tauri::Builder<tauri::Wry> {
-    builder.plugin(
+        // INFO: Commands que serão chamados via invoke
         // prettier-ignore
-        tauri_plugin_sql::Builder::new()
-            .add_migrations(
-                "sqlite:acerola.db",
-                crate::infra::db::migrations::get_migrations(),
-            )
-            .build(),
-    )
-}
+        builder.setup(setup_runtime).invoke_handler(
+                tauri::generate_handler![
+                    select_folder_cmd::select_folder,
+                    comic_scanner_cmd::comic_scanner
+                ]
+        )
+    }
 
-fn configure_all_plugins(builder: tauri::Builder<tauri::Wry>) -> tauri::Builder<tauri::Wry> {
-    let builder = setup_opener(builder);
-    let builder = setup_dialog(builder);
-    let builder = setup_store(builder);
-    let builder = setup_fs(builder);
-    setup_sql(builder)
+    fn setup_opener(builder: tauri::Builder<tauri::Wry>) -> tauri::Builder<tauri::Wry> {
+        builder.plugin(tauri_plugin_opener::init())
+    }
+
+    fn setup_dialog(builder: tauri::Builder<tauri::Wry>) -> tauri::Builder<tauri::Wry> {
+        builder.plugin(tauri_plugin_dialog::init())
+    }
+
+    fn setup_fs(builder: tauri::Builder<tauri::Wry>) -> tauri::Builder<tauri::Wry> {
+        builder.plugin(tauri_plugin_fs::init())
+    }
+
+    fn setup_store(builder: tauri::Builder<tauri::Wry>) -> tauri::Builder<tauri::Wry> {
+        builder.plugin(tauri_plugin_store::Builder::new().build())
+    }
+
+    fn setup_sql(builder: tauri::Builder<tauri::Wry>) -> tauri::Builder<tauri::Wry> {
+        builder.plugin(
+            // prettier-ignore
+            tauri_plugin_sql::Builder::new()
+                .add_migrations("sqlite:acerola.db", crate::infra::db::migrations::get_migrations()).build()
+        )
+    }
+
+    fn setup_runtime(app: &mut tauri::App) -> Result<(), Box<dyn std::error::Error>> {
+        let handle: tauri::AppHandle = app.handle().clone();
+        let (db_path, log_dir) = resolve_paths(app);
+
+        app.handle().plugin(
+            // prettier-ignore
+            tauri_plugin_log::Builder::new().target(
+                tauri_plugin_log::Target::new(tauri_plugin_log::TargetKind::Folder {
+                    path: log_dir,
+                    file_name: None,
+                })
+            ).level(tauri_plugin_log::log::LevelFilter::Info).build()
+        )?;
+
+        tauri::async_runtime::block_on(async move {
+            // prettier-ignore
+            let pool = sqlx::SqlitePool::connect(&format!("sqlite:{}?mode=rwc", db_path.to_string_lossy())).await.unwrap();
+            handle.manage(pool);
+        });
+
+        Ok(())
+    }
+
+    fn resolve_paths(app: &tauri::App) -> (PathBuf, PathBuf) {
+        let base = app.path().app_data_dir().unwrap();
+        let logs = base.join("logs");
+
+        std::fs::create_dir_all(&logs).unwrap();
+        (base.join("acerola.db"), logs)
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let builder = tauri::Builder::default().plugin(
-        tauri_plugin_log::Builder::new()
-            .level(tauri_plugin_log::log::LevelFilter::Info)
-            .build(),
-    );
-    let builder = configure_all_plugins(builder);
-
-    builder
-        .setup(|app| {
-            let handle = app.handle().clone();
-            // prettier-ignore
-            let db_path = app
-                .path()
-                .app_data_dir()
-                .expect("Failed to resolve app_data_dir")
-                .join("acerola.db");
-
-            tauri::async_runtime::block_on(async move {
-                let pool = sqlx::SqlitePool::connect(&format!(
-                    "sqlite:{}?mode=rwc",
-                    db_path.to_string_lossy()
-                ))
-                .await
-                .expect("Failed to connect to the database.");
-
-                handle.manage(pool);
-            });
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![
-            select_folder_cmd::select_folder,
-            comic_scanner_cmd::comic_scanner
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    // prettier-ignore
+    app_bootstrap::build().run(tauri::generate_context!()).expect("Error while running tauri application");
 }
