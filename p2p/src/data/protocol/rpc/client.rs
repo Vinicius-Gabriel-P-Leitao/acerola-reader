3a96041612c307fa4704c7a9568f8b10e8e67a21
Vinicius-Gabriel-P-Leitao <vinicius.gabriel.p.leitao@gmail.com>
2026-04-27 19:44:46 -0300

    [refactor](p2p): Organiza protocolo RPC de descoberta em mais arquivos.

---
+++ p2p/src/data/protocol/rpc/client.rs
@@ -0,0 +1,235 @@
+use async_trait::async_trait;
+use std::sync::Arc;
+use tokio::io::{AsyncRead, AsyncWrite};
+use tokio::sync::RwLock;
+use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
+
+use super::{read_byte, read_device_info, write_byte, write_device_info, Recv, Writer, PING, PONG};
+use crate::core::network::state::NetworkState;
+use crate::data::identity::device_info::DeviceInfo;
+use crate::data::protocol::{EventEmitter, ProtocolHandler};
+use crate::infra::error::ConnectionError;
+use crate::infra::peer::PeerId;
+
+pub struct RpcClientHandler {
+    emit: EventEmitter,
+    local_info: DeviceInfo,
+    state: Arc<RwLock<NetworkState>>,
+}
+
+#[async_trait]
+impl ProtocolHandler for RpcClientHandler {
+    async fn handle(
+        &self, peer: &PeerId, send: Box<dyn AsyncWrite + Send + Unpin>,
+        recv: Box<dyn AsyncRead + Send + Unpin>,
+    ) -> Result<(), ConnectionError> {
+        let mut framed_recv = FramedRead::new(recv, LengthDelimitedCodec::new());
+        let mut framed_send = FramedWrite::new(send, LengthDelimitedCodec::new());
+
+        self.perform_handshake(peer, &mut framed_send, &mut framed_recv).await?;
+        self.exchange_device_info(peer, &mut framed_send, &mut framed_recv).await?;
+        self.protocol_loop(&mut framed_send, &mut framed_recv).await?;
+
+        Ok(())
+    }
+}
+
+impl RpcClientHandler {
+    pub fn new(
+        emit: EventEmitter, local_info: DeviceInfo, state: Arc<RwLock<NetworkState>>,
+    ) -> Self {
+        Self { emit, local_info, state }
+    }
+
+    async fn perform_handshake(
+        &self, peer: &PeerId, send: &mut Writer, recv: &mut Recv,
+    ) -> Result<(), ConnectionError> {
+        write_byte(send, PING).await?;
+        (self.emit)("rpc:ping_sent", peer.id.clone());
+
+        match read_byte(recv).await {
+            Ok(PONG) => {
+                log::debug!("[RpcClient] pong from {}", peer.id);
+                (self.emit)("rpc:pong_received", peer.id.clone());
+                Ok(())
+            },
+            Ok(_) => Err(ConnectionError::StreamFailed("unexpected response to ping".into())),
+            Err(err) => Err(ConnectionError::from(err)),
+        }
+    }
+
+    async fn exchange_device_info(
+        &self, peer: &PeerId, send: &mut Writer, recv: &mut Recv,
+    ) -> Result<(), ConnectionError> {
+        write_device_info(send, &self.local_info).await?;
+        (self.emit)("rpc:device_info_sent", peer.id.clone());
+
+        match read_device_info(recv).await {
+            Ok(device_info) => {
+                log::debug!("[RpcClient] device info from {}", peer.id);
+                (self.emit)("rpc:device_info_received", peer.id.clone());
+                self.state.write().await.store_device_info(peer.clone(), device_info);
+                Ok(())
+            },
+            Err(err) => Err(ConnectionError::from(err)),
+        }
+    }
+
+    async fn protocol_loop(
+        &self, send: &mut Writer, recv: &mut Recv,
+    ) -> Result<(), ConnectionError> {
+        loop {
+            match read_byte(recv).await {
+                Ok(PING) => {
+                    write_byte(send, PONG).await?;
+                },
+                Ok(_) => break,
+                Err(err) => return Err(ConnectionError::from(err)),
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::core::network::state::NetworkState;
+    use crate::data::protocol::EventEmitter;
+    use crate::infra::peer::PeerId;
+    use std::sync::{Arc, Mutex};
+    use tokio::sync::RwLock;
+    use tokio::time::{sleep, Duration};
+
+    fn make_peer(id: &str) -> PeerId {
+        PeerId { id: id.to_string(), device_id: None }
+    }
+
+    fn make_device_info(name: &str) -> DeviceInfo {
+        DeviceInfo { name: name.to_string(), os: "linux".to_string(), version: "0.0.1".to_string() }
+    }
+
+    fn make_state() -> Arc<RwLock<NetworkState>> {
+        Arc::new(RwLock::new(NetworkState::new()))
+    }
+
+    fn capture_emitter() -> (EventEmitter, Arc<Mutex<Vec<String>>>) {
+        let events = Arc::new(Mutex::new(Vec::new()));
+        let clone = Arc::clone(&events);
+
+        let emit: EventEmitter = Arc::new(move |event: &str, _: String| {
+            clone.lock().unwrap().push(event.to_string());
+        });
+
+        (emit, events)
+    }
+
+    #[tokio::test]
+    async fn cliente_envia_ping_primeiro() {
+        let (client_side, server_side) = tokio::io::duplex(4096);
+        let (emit, events) = capture_emitter();
+        let client = RpcClientHandler::new(emit, make_device_info("client"), make_state());
+
+        let peer = make_peer("peer-1");
+        let peer_c = peer.clone();
+        tokio::spawn(async move {
+            let (read, write) = tokio::io::split(client_side);
+            let _ = client.handle(&peer_c, Box::new(write), Box::new(read)).await;
+        });
+
+        use super::super::{
+            read_byte, read_device_info, write_byte, write_device_info, PING, PONG,
+        };
+        use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
+        let (read, write) = tokio::io::split(server_side);
+        let mut recv = FramedRead::new(
+            Box::new(read) as Box<dyn tokio::io::AsyncRead + Send + Unpin>,
+            LengthDelimitedCodec::new(),
+        );
+        let mut send = FramedWrite::new(
+            Box::new(write) as Box<dyn tokio::io::AsyncWrite + Send + Unpin>,
+            LengthDelimitedCodec::new(),
+        );
+
+        let byte = read_byte(&mut recv).await.unwrap();
+        assert_eq!(byte, PING);
+
+        write_byte(&mut send, PONG).await.unwrap();
+        write_device_info(&mut send, &make_device_info("server")).await.unwrap();
+        read_device_info(&mut recv).await.unwrap();
+
+        sleep(Duration::from_millis(20)).await;
+        assert!(events.lock().unwrap().iter().any(|e| e == "rpc:ping_sent"));
+        assert!(events.lock().unwrap().iter().any(|e| e == "rpc:pong_received"));
+    }
+
+    #[tokio::test]
+    async fn cliente_encerra_se_receber_byte_errado_apos_ping() {
+        let (client_side, server_side) = tokio::io::duplex(4096);
+        let (emit, _) = capture_emitter();
+        let client = RpcClientHandler::new(emit, make_device_info("client"), make_state());
+
+        let peer = make_peer("peer-1");
+        let client_task = tokio::spawn(async move {
+            let (read, write) = tokio::io::split(client_side);
+            client.handle(&peer, Box::new(write), Box::new(read)).await
+        });
+
+        use super::super::{read_byte, write_byte};
+        use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
+        let (read, write) = tokio::io::split(server_side);
+        let mut recv = FramedRead::new(
+            Box::new(read) as Box<dyn tokio::io::AsyncRead + Send + Unpin>,
+            LengthDelimitedCodec::new(),
+        );
+        let mut send = FramedWrite::new(
+            Box::new(write) as Box<dyn tokio::io::AsyncWrite + Send + Unpin>,
+            LengthDelimitedCodec::new(),
+        );
+
+        read_byte(&mut recv).await.unwrap();
+        write_byte(&mut send, 0xFF).await.unwrap();
+
+        sleep(Duration::from_millis(20)).await;
+        assert!(client_task.is_finished());
+        assert!(client_task.await.unwrap().is_err());
+    }
+
+    #[tokio::test]
+    async fn cliente_armazena_device_info_do_servidor() {
+        let (client_side, server_side) = tokio::io::duplex(4096);
+        let (emit, _) = capture_emitter();
+        let state = make_state();
+        let client = RpcClientHandler::new(emit, make_device_info("client"), Arc::clone(&state));
+
+        let peer = make_peer("peer-1");
+        let peer_c = peer.clone();
+        tokio::spawn(async move {
+            let (read, write) = tokio::io::split(client_side);
+            let _ = client.handle(&peer_c, Box::new(write), Box::new(read)).await;
+        });
+
+        use super::super::{read_byte, read_device_info, write_byte, write_device_info, PONG};
+        use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
+        let (read, write) = tokio::io::split(server_side);
+        let mut recv = FramedRead::new(
+            Box::new(read) as Box<dyn tokio::io::AsyncRead + Send + Unpin>,
+            LengthDelimitedCodec::new(),
+        );
+        let mut send = FramedWrite::new(
+            Box::new(write) as Box<dyn tokio::io::AsyncWrite + Send + Unpin>,
+            LengthDelimitedCodec::new(),
+        );
+
+        read_byte(&mut recv).await.unwrap();
+        write_byte(&mut send, PONG).await.unwrap();
+        write_device_info(&mut send, &make_device_info("server-pc")).await.unwrap();
+        read_device_info(&mut recv).await.unwrap();
+
+        sleep(Duration::from_millis(30)).await;
+
+        let stored = state.read().await.get_device_info(&peer).map(|d| d.name.clone());
+        assert_eq!(stored, Some("server-pc".to_string()));
+    }
+}
