93ca6478305f2f851b0bbdefe205b6bcf0a215ba
Vinicius-Gabriel-P-Leitao <vinicius.gabriel.p.leitao+github@gmail.com>
2026-07-19 15:08:22 -0300

    [fixing](desktop): Corrigindo passagem de argumentos para o Webview2 via wdio capabilities.

--- desktop/wdio.conf.ts
+++ desktop/wdio.conf.ts
@@ -28,6 +28,12 @@ const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 const capabilities: TauriCapability[] = [
 	{
 		browserName: process.platform === 'win32' ? 'chrome' : 'wry',
+		'goog:chromeOptions': {
+			args: ['--no-sandbox', '--disable-gpu']
+		},
+		'ms:edgeOptions': {
+			args: ['--no-sandbox', '--disable-gpu']
+		},
 		'tauri:options': {
 			application: appPath
 		}
@@ -86,7 +92,10 @@ export const config: Options.Testrunner & Capabilities.WithRequestedTestrunnerCa
 		tauriDriver = driver;
 
 		driver.stderr.on('data', (chunk) => {
-			process.stderr.write(`[tauri-driver] ${chunk}`);
+			process.stderr.write(`[tauri-driver STDERR] ${chunk}`);
+		});
+		driver.stdout.on('data', (chunk) => {
+			process.stdout.write(`[tauri-driver STDOUT] ${chunk}`);
 		});
 
 		driver.on('error', (error) => {
